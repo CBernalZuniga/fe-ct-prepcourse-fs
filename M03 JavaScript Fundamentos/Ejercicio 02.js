6ffbf463c7d29af21584acbab1930488ef7ae7f1
Hakai <hakaifire@proton.me>
2023-09-04 23:08:01 -0500

    Progreso Ejercicios M03 EJ(01-02)

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -12,27 +12,38 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return(x+y);
 }
+suma(8,4);
+
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
+   // Tu código: 
+   return (x-y);
 }
+resta(8,4);
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return (x/y);
 }
-
+divide(8,4)
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return (x*y);
 }
+multiplica(8,4)
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return (x%y);
 }
+obtenerResto(8%4);
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
