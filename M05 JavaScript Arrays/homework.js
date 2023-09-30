a719aa59acc3d3b49bca99524cfcb78a3fcb37ea
Hakai <hakaifire@proton.me>
2023-09-30 16:50:00 -0500

    HW M 05 EN PRG

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -71,7 +71,6 @@ function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
- 
    var suma = 0;
    for (var i = 0; i< arrayOfNums.length; i++)
    {suma += arrayOfNums[i];}
@@ -84,13 +83,25 @@ function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   var suma = 0;
    
-}
+   for (var i= 0; i < resultadosTest.length; i++)
+   {suma += resultadosTest[i];}
+   var promedio = suma/resultadosTest.length;
+   return promedio;
+} 
+
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   var maximo = arrayOfNums[0];
+   for (var i = 1; i < arrayOfNums.length; i++)
+   {if( arrayOfNums[i] > maximo) {maximo = arrayOfNums[i];
+   } 
+}
+   return maximo;
 }
 
 function multiplicarArgumentos() {
@@ -98,11 +109,22 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   if(arguments.length === 0) 
+   {return 0;}
+   var producto = 1;
+   for (var i=0; i < arguments.length; i++)
+   {producto *=arguments[i];}
+   return producto;
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   var contador = 0;
+   for (var i = 0; i < array.length; i++ )
+   {if (array[i] > 18) {contador++;}
+}
+return contador;
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -110,6 +132,11 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+
+   if (numeroDeDia === 1 || numeroDeDia === 7)
+   {return "Es fin de semana"; }
+   else {return "Es dia laboral";}
+
 }
 
 function empiezaConNueve(num) {
